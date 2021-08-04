f39a95114933b477fda265ce5aaf064b442c6446
Mikhaiel_Offical <81913716+Mikhaiel@users.noreply.github.com>
2021-08-04 14:56:29 +0530

    Create Owner.js

---
+++ plugins/Owner.js
@@ -0,0 +1,29 @@
+const Asena = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
+
+const Language = require('../language');
+const Lang = Language.getString('wallpaper');
+
+Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message, match) => {
+
+    var r_text = new Array ();
+    
+    
+   
+  r_text[0] = "https://c4.wallpaperflare.com/wallpaper/195/797/214/aladdin-aladdin-gin-hd-wallpaper-preview.jpg";
+    
+    
+    var i = Math.floor(1*Math.random())
+
+    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
+
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Creater Mikhaiel*
+*Bgm updation💘*
+*Owner number wa.me/919544846609*
+*githublink       https://github.com/Mikhaiel/Jinnh*
+*audio commads    https://github.com/Mikhaiel/Jinnh/tree/master/uploads*
+*sticker commads  https://github.com/Mikhaiel/Jinnh/tree/master/stickers*
+`}) 
+
+}));
